3b9bcfb44650afe8315ec2e6d00481b8ef1d2c91
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-03-21 21:28:35 +0530

    retweet

--- routes/retweet.js
+++ routes/retweet.js
@@ -1,6 +1,26 @@
-var express = require("express");
-var router = express.Router();
-
+const config = require("../config");
+const twit = require("twit");
+const Twitter = new twit(config.twitter);
 /* GET home page. */
+const retweet = () => {
+	let prams = {
+		query: config.twitterConfig.queryString,
+		result_type: "recent",
+		lang: "en"
+	};
+};
+Twitter.get("search/tweets", params, (err, data, response) => {
+	if (err) console.log("ERROR: Cannot search tweet");
+	let retweetId = data.search_metadata.max_id_str;
+	console.log(data.search_metadata.max_id_str);
 
-module.exports = router;
+	Twitter.post("statuses/retweet/:id", { id: retweetId }, (err, response) => {
+		if (err) {
+			console.log("ERROR:", err);
+		} else if (response) {
+			console.log("SUCCESS: Retweet Successfull");
+		}
+	});
+});
+
+module.exports = retweet;
