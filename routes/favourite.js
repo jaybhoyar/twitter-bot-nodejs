3ec446aac0fa9727050039cc4f1507909b1cc0bf
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-03-22 23:23:29 +0530

    favourite js

--- routes/favourite.js
+++ routes/favourite.js
@@ -1,6 +1,27 @@
-var express = require("express");
-var router = express.Router();
+const config = require("./config.js");
+const twit = require("twit");
+const Twitter = new twit(config.twitter);
 
-/* GET users listing. */
+const favorite = () => {
+	let params = {
+		query: config.twitterConfig.queryString,
+		result_type: "recent",
+		lang: "en"
+	};
 
-module.exports = router;
+	Twitter.get("search/tweets", params, (err, data) => {
+		if (err) {
+			console.log("ERROR: Cannot search tweet");
+		}
+		let tweetId = data.search_metadata.max_id_str;
+
+		Twitter.post("favorites/create", { id: tweetId }, (err, response) => {
+			if (err) {
+				console.log("ERROR: in Retweeting");
+			}
+			console.log(response);
+		});
+	});
+};
+
+module.exports = favorite;
--- routes/retweet.js
+++ routes/retweet.js
@@ -2,25 +2,31 @@ const config = require("../config");
 const twit = require("twit");
 const Twitter = new twit(config.twitter);
 /* GET home page. */
+
 const retweet = () => {
-	let prams = {
+	let params = {
 		query: config.twitterConfig.queryString,
 		result_type: "recent",
 		lang: "en"
 	};
-};
-Twitter.get("search/tweets", params, (err, data, response) => {
-	if (err) console.log("ERROR: Cannot search tweet");
-	let retweetId = data.search_metadata.max_id_str;
-	console.log(data.search_metadata.max_id_str);
 
-	Twitter.post("statuses/retweet/:id", { id: retweetId }, (err, response) => {
-		if (err) {
-			console.log("ERROR:", err);
-		} else if (response) {
-			console.log("SUCCESS: Retweet Successfull");
-		}
+	Twitter.get("search/tweets", params, (err, data) => {
+		if (err) console.log("ERROR: Cannot search tweet");
+		let retweetId = data.search_metadata.max_id_str;
+		console.log(data.search_metadata.max_id_str);
+
+		Twitter.post(
+			"statuses/retweet/:id",
+			{ id: retweetId },
+			(err, response) => {
+				if (err) {
+					console.log("ERROR:", err);
+				} else if (response) {
+					console.log("SUCCESS: Retweet Successfull");
+				}
+			}
+		);
 	});
-});
+};
 
 module.exports = retweet;
